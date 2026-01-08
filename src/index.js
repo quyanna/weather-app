c0ad5ce5494e05dd653f3e5a5c4a48f0b647e1d8
quyanna <quyannacampbell@gmail.com>
2026-01-07 20:31:55 -0800

    Refactor back into one async function

--- src/index.js
+++ src/index.js
@@ -13,29 +13,24 @@ const myLocation = "Vancouver";
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
+  displayDiv.textContent = "loading...";
   const data = new FormData(e.target);
   const formLocation = data.get("location");
-  await writeWeather(formLocation);
+  displayDiv.textContent = await fetchWeatherData(formLocation);
 });
 
-async function writeWeather(location) {
-  try {
-    displayDiv.textContent = "Loading...";
-    const weather = await fetchWeatherData(location);
-    displayDiv.textContent = JSON.stringify(weather, null, "\t");
-  } catch (error) {
-    displayDiv.textContent = `Failed to write weather data (${error})`;
-  }
-}
-
 async function fetchWeatherData(location) {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days%2Ccurrent%2Calerts&key=X7MF6JQ3HQAZ7UEMLUSVFSNPV&contentType=json`
-  );
-
-  //if there is an HTTP Error
-  if (!response.ok) {
-    throw new Error(`${response.status}`);
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days%2Ccurrent%2Calerts&key=X7MF6JQ3HQAZ7UEMLUSVFSNPV&contentType=json`
+    );
+    //if there is an HTTP Error
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    const jsonData = await response.json();
+    return JSON.stringify(jsonData);
+  } catch (error) {
+    return `There was a problem fetching weather data (${error})`;
   }
-  return await response.json();
 }
