e83986aeb79a97270fe3a78492aa4871916b60fa
quyanna <quyannacampbell@gmail.com>
2026-01-07 19:26:03 -0800

    Add function to fetch weather data from a given location

--- src/index.js
+++ src/index.js
@@ -5,3 +5,27 @@ import "./styles.css";
 // import odinImage from "./odin.png";
 
 console.log("Hello, world!");
+
+const displayDiv = document.querySelector(".weather-data");
+const myLocation = "Vancouver";
+
+async function getWeatherData(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days%2Ccurrent%2Calerts&key=X7MF6JQ3HQAZ7UEMLUSVFSNPV&contentType=json`
+    );
+
+    //if there is an HTTP Error
+    if (!response.ok) {
+      throw new Error(`HTTP Error: Status ${response.status}`);
+    }
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    console.error("Error fetching weather data", error);
+    return null;
+  }
+}
+
+const weather = await getWeatherData(myLocation);
+displayDiv.textContent = JSON.stringify(weather, null, "\t");
